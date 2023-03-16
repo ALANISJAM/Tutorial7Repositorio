2a3cb78e29a0b168ea137f804e0babde367f7435
ALANISJAM <jalanismende@gmail.com>
2023-03-16 12:30:15 -0600

    ahora si ya

--- Tutorial07/src/Device.cpp
+++ Tutorial07/src/Device.cpp
@@ -22,8 +22,8 @@ Device::destroy() {
 
 HRESULT
 Device::CreateDepthStencilView(ID3D11Resource* pResource,
-	                           const D3D11_DEPTH_STENCIL_VIEW_DESC* pDesc,
-	                           ID3D11DepthStencilView** ppDepthStencilView) {
+                               const D3D11_DEPTH_STENCIL_VIEW_DESC* pDesc,
+                               ID3D11DepthStencilView** ppDepthStencilView) {
 	HRESULT hr = S_OK;
 
 	if (pResource == nullptr) {
@@ -44,8 +44,8 @@ Device::CreateDepthStencilView(ID3D11Resource* pResource,
 
 HRESULT
 Device::CreateRenderTargetView(ID3D11Resource* pResource,
-	                           const D3D11_RENDER_TARGET_VIEW_DESC* pDesc,
-	                           ID3D11RenderTargetView** ppRTView) {
+                               const D3D11_RENDER_TARGET_VIEW_DESC* pDesc,
+                               ID3D11RenderTargetView** ppRTView) {
 	HRESULT hr = S_OK;
 
 	if (pResource == nullptr) {
@@ -66,8 +66,8 @@ Device::CreateRenderTargetView(ID3D11Resource* pResource,
 
 HRESULT
 Device::CreateTexture2D(const D3D11_TEXTURE2D_DESC* pDesc,
-	                    const D3D11_SUBRESOURCE_DATA* pInitialData,
-	                    ID3D11Texture2D** ppTexture2D) {
+                        const D3D11_SUBRESOURCE_DATA* pInitialData,
+                        ID3D11Texture2D** ppTexture2D) {
 	HRESULT hr = S_OK;
 
 	if (pDesc == nullptr) {
@@ -88,9 +88,9 @@ Device::CreateTexture2D(const D3D11_TEXTURE2D_DESC* pDesc,
 
 HRESULT
 Device::CreateVertexShader(const void* pShaderBytecode,
-	                       unsigned int BytecodeLength,
-	                       ID3D11ClassLinkage* pClassLinkage,
-	                       ID3D11VertexShader** ppVertexShader) {
+                           unsigned int BytecodeLength,
+                           ID3D11ClassLinkage* pClassLinkage,
+                           ID3D11VertexShader** ppVertexShader) {
 	HRESULT hr = S_OK;
 
 	if (pShaderBytecode == nullptr) {
